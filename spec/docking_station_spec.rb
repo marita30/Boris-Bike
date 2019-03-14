cd8c8d6487f34a50714c21f187b8d2e457e82bd3
M.J <marita.jose.ch@gmail.com>
2019-03-14 22:35:57 +0100

    step 14 listo con error del step 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,13 +5,14 @@ describe DockingStation do
 
 	describe '#release_bike' do
 		it 'raises an error when there are no bikes available' do
+
 			expect { subject.release_bike }.to raise_error 'No bikes available'
 		end
 	end
 
 	describe '#dock' do
 		it 'raises an error when full' do
-			subject.dock(Bike.new)
+			20.times {subject.dock Bike.new
 			expect {subject.dock Bike.new}.to raise_error 'Docking station full'
 		end
 	end
@@ -31,7 +32,5 @@ describe DockingStation do
 		expect(subject.bike).to eq bike
 
 	end
-
-
 end
   
